8d00f45a36ad69267fbe4ada2b49067c98223cb5
Mark <mark@remarkablemark.org>
2019-07-16 23:55:49 -0400

    test(index): add more test cases

--- test/index.js
+++ test/index.js
@@ -1,6 +1,9 @@
 const assert = require('assert');
 const tried = require('..');
 
+const KEY = '$$'; // end of word key
+const VALUE = 1; // end of word value
+
 describe('tried', () => {
   [undefined, null, 0, 1, 'string'].forEach(arg => {
     it(`returns {} when arguments=[${arg}]`, () => {
@@ -16,8 +19,92 @@ describe('tried', () => {
 });
 
 [
-  [['a'], { a: { $$: 1 } }],
-  [['a', 'ab'], { a: { $$: 1, b: { $$: 1 } } }]
+  /**
+   *  a
+   *  |
+   *  .
+   */
+  [
+    ['a'],
+    {
+      a: { [KEY]: VALUE }
+    }
+  ],
+
+  /**
+   *    a
+   *   / \
+   *  .   b
+   *      |
+   *      .
+   */
+  [
+    ['a', 'ab'],
+    {
+      a: {
+        [KEY]: VALUE,
+        b: { [KEY]: VALUE }
+      }
+    }
+  ],
+
+  /**
+   *     a
+   *   / | \
+   *  .  b  d
+   *     |  |
+   *     .  .
+   */
+  [
+    ['a', 'ab', 'ad'],
+    {
+      a: {
+        [KEY]: VALUE,
+        b: { [KEY]: VALUE },
+        d: { [KEY]: VALUE }
+      }
+    }
+  ],
+
+  /**
+   *    a
+   *    |
+   *    n
+   *   / \
+   *  .   n
+   *     / \
+   *    .   a
+   *        |
+   *        .
+   */
+  [
+    ['an', 'ann', 'anna'],
+    {
+      a: {
+        n: {
+          [KEY]: VALUE,
+          n: {
+            [KEY]: VALUE,
+            a: { [KEY]: VALUE }
+          }
+        }
+      }
+    }
+  ],
+
+  /**
+   *  a  b  c
+   *  |  |  |
+   *  .  .  .
+   */
+  [
+    ['a', 'b', 'c'],
+    {
+      a: { [KEY]: VALUE },
+      b: { [KEY]: VALUE },
+      c: { [KEY]: VALUE }
+    }
+  ]
 ].forEach(testCase => {
   const [args, expected] = testCase;
 
