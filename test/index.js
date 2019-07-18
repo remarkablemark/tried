c8821010b20d45ebd38cc607d397cc2bede9bbff
Mark <mark@remarkablemark.org>
2019-07-18 01:18:09 -0400

    test(index): organize tests under suite `data`

--- test/index.js
+++ test/index.js
@@ -4,113 +4,117 @@ const tried = require('..');
 const KEY = '$$'; // end of word key
 const VALUE = 1; // end of word value
 
-describe('tried', () => {
+describe('data', () => {
   [undefined, null, 0, 1, 'string'].forEach(arg => {
-    it(`sets data={} when arguments=[${arg}]`, () => {
-      assert.deepEqual(tried(arg).data, {});
+    describe(`when arguments=[${arg}]`, () => {
+      it('returns {}', () => {
+        assert.deepEqual(tried(arg).data, {});
+      });
     });
   });
 
   [{}, [], () => {}, new Date()].forEach(arg => {
-    it(`sets data={} when arguments=[${arg.constructor.name}]`, () => {
-      assert.deepEqual(tried(arg).data, {});
+    describe(`when arguments=[${arg.constructor.name}]`, () => {
+      it('returns {}', () => {
+        assert.deepEqual(tried(arg).data, {});
+      });
     });
   });
-});
 
-[
-  /**
-   *  a
-   *  |
-   *  .
-   */
   [
-    ['a'],
-    {
-      a: { [KEY]: VALUE }
-    }
-  ],
-
-  /**
-   *    a
-   *   / \
-   *  .   b
-   *      |
-   *      .
-   */
-  [
-    ['a', 'ab'],
-    {
-      a: {
-        [KEY]: VALUE,
-        b: { [KEY]: VALUE }
+    /**
+     *  a
+     *  |
+     *  .
+     */
+    [
+      ['a'],
+      {
+        a: { [KEY]: VALUE }
       }
-    }
-  ],
+    ],
 
-  /**
-   *     a
-   *   / | \
-   *  .  b  d
-   *     |  |
-   *     .  .
-   */
-  [
-    ['a', 'ab', 'ad'],
-    {
-      a: {
-        [KEY]: VALUE,
-        b: { [KEY]: VALUE },
-        d: { [KEY]: VALUE }
-      }
-    }
-  ],
-
-  /**
-   *    a
-   *    |
-   *    n
-   *   / \
-   *  .   n
-   *     / \
-   *    .   a
-   *        |
-   *        .
-   */
-  [
-    ['an', 'ann', 'anna'],
-    {
-      a: {
-        n: {
+    /**
+     *    a
+     *   / \
+     *  .   b
+     *      |
+     *      .
+     */
+    [
+      ['a', 'ab'],
+      {
+        a: {
           [KEY]: VALUE,
+          b: { [KEY]: VALUE }
+        }
+      }
+    ],
+
+    /**
+     *     a
+     *   / | \
+     *  .  b  d
+     *     |  |
+     *     .  .
+     */
+    [
+      ['a', 'ab', 'ad'],
+      {
+        a: {
+          [KEY]: VALUE,
+          b: { [KEY]: VALUE },
+          d: { [KEY]: VALUE }
+        }
+      }
+    ],
+
+    /**
+     *    a
+     *    |
+     *    n
+     *   / \
+     *  .   n
+     *     / \
+     *    .   a
+     *        |
+     *        .
+     */
+    [
+      ['an', 'ann', 'anna'],
+      {
+        a: {
           n: {
             [KEY]: VALUE,
-            a: { [KEY]: VALUE }
+            n: {
+              [KEY]: VALUE,
+              a: { [KEY]: VALUE }
+            }
           }
         }
       }
-    }
-  ],
+    ],
 
-  /**
-   *  a  b  c
-   *  |  |  |
-   *  .  .  .
-   */
-  [
-    ['a', 'b', 'c'],
-    {
-      a: { [KEY]: VALUE },
-      b: { [KEY]: VALUE },
-      c: { [KEY]: VALUE }
-    }
-  ]
-].forEach(testCase => {
-  const [args, expected] = testCase;
+    /**
+     *  a  b  c
+     *  |  |  |
+     *  .  .  .
+     */
+    [
+      ['a', 'b', 'c'],
+      {
+        a: { [KEY]: VALUE },
+        b: { [KEY]: VALUE },
+        c: { [KEY]: VALUE }
+      }
+    ]
+  ].forEach(testCase => {
+    const [args, expected] = testCase;
 
-  describe(`when arguments=[${JSON.stringify(args)}]`, () => {
-    it(`sets data=${JSON.stringify(expected)}`, () => {
-      assert.deepEqual(tried(args).data, expected);
+    describe(`when arguments=[${JSON.stringify(args)}]`, () => {
+      it(`sets data=${JSON.stringify(expected)}`, () => {
+        assert.deepEqual(tried(args).data, expected);
+      });
     });
   });
 });
