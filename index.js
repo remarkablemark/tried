85a09d64db2a0f86f7e712836e2420ad29d667d8
Mark <mark@remarkablemark.org>
2019-08-28 16:36:33 -0400

    docs: add JSDoc params for `tried` and `Trie` options

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var Trie = require('./lib/trie');
  * Instantiates a trie.
  *
  * @param {Object} [options]
+ * @param {string} [options.endKey='$$']
+ * @param {number} [options.endValue=1]
  * @return {Trie}
  */
 function tried(options) {
